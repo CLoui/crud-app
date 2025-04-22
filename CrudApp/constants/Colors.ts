ed7148e01f58f99cf7c628d692a80aa99e0e4d25
Courtney Loui <cmloui@uwaterloo.ca>
2025-04-22 14:23:18 -0400

    Create light and dark themes, added animation

---
+++ CrudApp/constants/Colors.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+    light: {
+        text: '#11181C',
+        background: '#fff',
+        icon: '#687076',
+        button: 'royalblue',
+    },
+    dark: {
+        text: '#ECEDEE',
+        background: '#151718',
+        icon: '#9BA1A6',
+        button: 'royalblue',
+    },
+};
