ad8d68c1b31d65276c558cbeab10ae516a98190f
Courtney Loui <cmloui@uwaterloo.ca>
2025-04-22 12:31:15 -0400

    Create general functionality for to do list

---
+++ CrudApp/data/todos.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Finish book",
+        "completed": true
+    },
+    {
+        "id": 2,
+        "title": "Buy groceries",
+        "completed": true
+    },
+    {
+        "id": 3,
+        "title": "Wash clothes",
+        "completed": true
+    }
+]
